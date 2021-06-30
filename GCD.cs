fa4686004172e9296a1114b7bef2d6f889de4145
Sufiyan Shaikh <82521079+Sufiyan-S@users.noreply.github.com>
2021-06-30 18:31:15 +0530

    Update GCD.cs

--- GCD.cs
+++ GCD.cs
@@ -4,6 +4,7 @@ namespace TestProject
 {
     class Solution
     {
+      //Find find Commonn GCD factor among A and B
         public int gcd(int A, int B) {
             if (B == 0)
                 return A;
