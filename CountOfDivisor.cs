d2cc8460367878c8df0d460e016918e5a16dc9f7
Sufiyan Shaikh <82521079+Sufiyan-S@users.noreply.github.com>
2021-07-03 19:37:14 +0530

    Update CountOfDivisor.cs

--- CountOfDivisor.cs
+++ CountOfDivisor.cs
@@ -1,1 +1,24 @@
+class Solution {
+    public List<int> solve(List<int> A) {
+        List<int> divsor= new List<int>();
+        
+        for(int i=0;i<A.Count;i++)
+        {
+            int cnt = 0;
+            for(int j=1;j<=Math.Sqrt(A[i]);j++)
+            {
+                if(A[i] % j == 0)
+                {
+                 if (A[i] / j == j)
+                    cnt++;
+                else
+                    cnt = cnt + 2;   
+                }
+            }
+            divsor.Add(cnt);
+        }
+        
+        return divsor;
+    }
+}
 
