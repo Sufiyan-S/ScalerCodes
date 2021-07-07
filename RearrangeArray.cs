56efb4fa9649ad20fd2554a419ad36116d0ed7c0
Sufiyan Shaikh <82521079+Sufiyan-S@users.noreply.github.com>
2021-07-07 19:32:27 +0530

    Update RearrangeArray.cs

--- RearrangeArray.cs
+++ RearrangeArray.cs
@@ -2,6 +2,12 @@ using System;
 using System.Collections.Generic;
 
 //Rearrange a given array so that Arr[i] becomes Arr[Arr[i]] with O(1) extra space.
+/*
+Example:
+
+Input : [1, 0]
+Return : [0, 1]*/
+
 class Solution {
     public void arrange(List<int> A) {
         int n=A.Count;
