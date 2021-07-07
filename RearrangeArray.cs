9f52fb370ef4d362ebb00b19aee01bf7b3587a93
Sufiyan Shaikh <82521079+Sufiyan-S@users.noreply.github.com>
2021-07-07 19:31:50 +0530

    Update RearrangeArray.cs

--- RearrangeArray.cs
+++ RearrangeArray.cs
@@ -1,1 +1,14 @@
+using System;
+using System.Collections.Generic;
 
+class Solution {
+    public void arrange(List<int> A) {
+        int n=A.Count;
+        for (int i = 0; i < n; i++)
+            A[i] += (A[A[i]] % n) * n;
+ 
+        // Second Step: Divide all values by n
+        for (int i = 0; i < n; i++)
+            A[i] /= n;
+    }
+}
