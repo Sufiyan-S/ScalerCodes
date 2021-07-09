7b0c3333422a4282c55499be9ecda57543958978
Sufiyan Shaikh <82521079+Sufiyan-S@users.noreply.github.com>
2021-07-09 17:28:53 +0530

    Update ReverseBits.cs

--- ReverseBits.cs
+++ ReverseBits.cs
@@ -2,12 +2,6 @@
 Problem Description
 Reverse the bits of an 32 bit unsigned integer A.
 
-Problem Constraints
-0 <= A <= 232
-
-Input Format
-First and only argument of input contains an integer A.
-
 Output Format
 Return a single unsigned integer denoting the decimal value of reversed bits.
 
@@ -24,9 +18,6 @@ Output 2:
  3221225472
 
 Example Explanation
-Explanation 1:
-        00000000000000000000000000000000    
-=>      00000000000000000000000000000000
 Explanation 2:
         00000000000000000000000000000011    
 =>      11000000000000000000000000000000
