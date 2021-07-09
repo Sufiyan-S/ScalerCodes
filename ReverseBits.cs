85cc9aef6a2d2aba7f35b017d23a614a87ef08b5
Sufiyan Shaikh <82521079+Sufiyan-S@users.noreply.github.com>
2021-07-09 17:26:48 +0530

    Update ReverseBits.cs

--- ReverseBits.cs
+++ ReverseBits.cs
@@ -1,1 +1,12 @@
+class Solution {
+    public long solve(long A) {
+        long rev = 0;
+        for(int i = 0; i < 32 ; i++) {
+            rev <<= 1;
+            if( (A & (1 << i) ) != 0)
+            rev |= 1;
+        }
+        return rev;
+    }
+}
 
