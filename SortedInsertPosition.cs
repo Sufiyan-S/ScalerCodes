33992559029a87436daed5dbc831c0ad9a4cb2f4
Sufiyan Shaikh <82521079+Sufiyan-S@users.noreply.github.com>
2021-07-13 17:38:12 +0530

    Update SortedInsertPosition.cs

--- SortedInsertPosition.cs
+++ SortedInsertPosition.cs
@@ -1,5 +1,18 @@
 class Solution {
     public int searchInsert(List<int> A, int B) {
+        int start  = 0, end = A.Count -1, mid;
+        
+        while(start <= end){
+            mid = (end - start)/2 + start;
+
+            if(A[mid] == B) 
+                return mid;
+            else if(mid < B)
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+        return end + 1;
     }
 }
 
