ff12a631f42648530838843545bbe6504a6d7927
Sufiyan Shaikh <82521079+Sufiyan-S@users.noreply.github.com>
2021-07-14 17:57:48 +0530

    Update FindPeakElement.cs

--- FindPeakElement.cs
+++ FindPeakElement.cs
@@ -1,4 +1,27 @@
 class Solution {
-    public int search(List<int> A) {
+    public int solve(List<int> A) {
+        if(A.Count == 0)
+            return 0;
+        if(A.Count == 1)
+            return A[0];
+        for(int i = 0; i < A.Count; i ++)
+        {
+            if(i == 0 )
+            {
+                if(A[0] >= A[1])
+                    return A[0];
+            }
+            else if(i == A.Count - 1)
+            {
+                if(A[A.Count -1] >= A[A.Count -2])
+                    return A[A.Count -1];
+            }
+            else
+            {
+                if(A[i] >= A[i-1] && A[i] >= A[i +1])
+                    return A[i];
+            }
+        }
+        return 0;
     }
 }
