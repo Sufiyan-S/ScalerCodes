11b3002b70175ea00ae19c8e30b83bf8b87bef29
Sufiyan Shaikh <82521079+Sufiyan-S@users.noreply.github.com>
2021-07-16 20:00:07 +0530

    Update SmallestGoodBase.cs

--- SmallestGoodBase.cs
+++ SmallestGoodBase.cs
@@ -1,4 +1,17 @@
 class Solution {
     public string solve(string A) {
+        long a = long.Parse(A);
+            int x = 1;
+            long res = 0;
+            for (int i = 62; i >= 2; i--)
+            {
+                if ((x << i) < a)
+                {
+                    res = computeGoodBase(a, i);
+                    if (res != -1)
+                        return res.ToString();
+                }
+            }
+            return (a - 1).ToString();
         }
 }
