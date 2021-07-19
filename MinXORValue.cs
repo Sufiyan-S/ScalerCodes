e8db17540776079730d0b6bea4059bf8c0599467
Sufiyan Shaikh <82521079+Sufiyan-S@users.noreply.github.com>
2021-07-19 19:06:51 +0530

    Update MinXORValue.cs

--- MinXORValue.cs
+++ MinXORValue.cs
@@ -1,4 +1,14 @@
 class Solution {
     public int findMinXor(List<int> A) {
+                A.Sort();
+        int x=0;
+        int min =int.MaxValue;
+        for(int i=0;i<A.Count-1;i++)
+        {
+            x= A[i]^A[i+1];
+            if(x < min)
+                min =x;
+        }
+        return min;
     }
 }
