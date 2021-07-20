c5ee376c6681049e1a3d82348890b9ad8799b7b9
Sufiyan Shaikh <82521079+Sufiyan-S@users.noreply.github.com>
2021-07-20 14:17:41 +0530

    Update MaximumAbsoluteDifference.cs

--- MaximumAbsoluteDifference.cs
+++ MaximumAbsoluteDifference.cs
@@ -1,4 +1,16 @@
 class Solution {
     public int maxArr(List<int> A) {
+        if(A.Count == 0)
+            return 0;
+        int sum = int.MinValue, max1 = int.MinValue, max2 = int.MinValue, min1 = int.MaxValue, min2 = int.MaxValue;
+        for (int i = 0; i < A.Count; i++)
+        {
+            max1 = Math.Max(max1, A[i] + i);
+            max2 = Math.Max(max2, A[i] - i);
+            min1 = Math.Min(min1, A[i] + i);
+            min2 = Math.Min(min2, A[i] - i);
+        }
+        sum = Math.Max(max1-min1, max2-min2);
+        return sum;
     }
 }
